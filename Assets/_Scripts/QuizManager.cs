9630a99b8612df3fc1d72e5584a8e71cc1461061
shannonpeelor <shannonpeelor@gmail.com>
2025-10-12 07:32:00 -0400

    Questions got BUFF

--- Assets/_Scripts/QuizManager.cs
+++ Assets/_Scripts/QuizManager.cs
@@ -41,7 +41,6 @@ public class QuizManager : MonoBehaviour
         PopulateAllThemes();
         SwitchToTheme(0);
         UpdateTabStates();
-        finalExitButton.gameObject.SetActive(false);
     }
 
     void PopulateAllThemes()
@@ -49,55 +48,27 @@ public class QuizManager : MonoBehaviour
         for (int i = 0; i < 3; i++) { allThemePools.Add(new List<QuestionData>()); }
 
         // Theme 1 15 questions
-        allThemePools[0].Add(new QuestionData { question = "Glory to ____?\n\nA. Arstotzka\nB. Kunstler\nC. King Bob", answer = "C" });
-        allThemePools[0].Add(new QuestionData { question = "Is your current social credit score above 9999?\n\nA. True\nB. False", answer = "A" });
-        allThemePools[0].Add(new QuestionData { question = "Who is the current president of Mars?\n\nA. Eli\nB. The AI overlords", answer = "B" });
-        allThemePools[0].Add(new QuestionData { question = "Do you believe in equality and voting rights of Earth robots and Titan aliens?\n\nA. Yes\nB. No", answer = "A" });
-        allThemePools[0].Add(new QuestionData { question = "Which country do you have a citizenship of?\n\nA. Arztotzka\nB. Terrarian Union", answer = "B" });
-        allThemePools[0].Add(new QuestionData { question = "Are you enjoying this game?\n\nA.Yes\nB. No", answer = "A" });
-        allThemePools[0].Add(new QuestionData { question = "Have you never thought about death?\n\nA. Yes\nB. No", answer = "B" });
-        allThemePools[0].Add(new QuestionData { question = "Are you not a Wall-B model?\n\nA. Yes\nB. No", answer = "B" });
-        allThemePools[0].Add(new QuestionData { question = "Is your current net worth in robux below 10,000?\n\nA. Hell yea\nB. No", answer = "B" });
-        allThemePools[0].Add(new QuestionData { question = "Have you committed war crimes or acts of treason?\n\nA. Only war crimes\nB. neither\nC. Only acts of treason", answer = "B" });
-        allThemePools[0].Add(new QuestionData { question = "When did world war 3 occur?\n\nA. 2067\nB. 2107\n", answer = "A" });
-        allThemePools[0].Add(new QuestionData { question = "Do you support the immigration in crisis act of 2070?\n\nA.Yes\nB. No", answer = "A" });
-        allThemePools[0].Add(new QuestionData { question = "Can long-term immigration caused by climate change and resource shortages ¡ª mixed with new diets, pollution exposure, and constant stress in a new country ¡ª actually change people¡¯s genes or biology faster than scientists usually think evolution works?\n\nA.True\nC. False", answer = "B" });
-        allThemePools[0].Add(new QuestionData { question = "Did you ever experience attachment with a person or an object?\n\nA.Yes\nB. No", answer = "A" });
-        allThemePools[0].Add(new QuestionData { question = "Will you join the martian army to defend your nation?\n\nA.Yes\nB. No", answer = "A" });
+        allThemePools[0].Add(new QuestionData { question = "Is your current intergalactic credit score above 9999?\n\nA. True\nB. False", answer = "A" });
+        allThemePools[0].Add(new QuestionData { question = "Which country do you have a citizenship of?\n\nA. Polar Republic\nB. Terrarian Union", answer = "B" });
+        allThemePools[0].Add(new QuestionData { question = "Is your current net worth below 10,000 in credits per solar rotation?\n\nA. Yes \nB. No", answer = "B" });
+        allThemePools[0].Add(new QuestionData { question = "Have you committed any felonies or crimes against the current government?\n\nA. Yes \nB. no", answer = "B" });
+        allThemePools[0].Add(new QuestionData { question = "Did you loose a place of residence in a climate change related disaster?\n\nA.Yes\nB. No", answer = "B" });
+        allThemePools[0].Add(new QuestionData { question = "Will you join the martian army to defend your nation, as required by a G5 visa?\n\nA.Yes\nB. No", answer = "A" });
 
         // Theme 2 15 questions
-        allThemePools[1].Add(new QuestionData { question = "Is Mars a chocolate or a Planet?\n\nA. Chocolate\nB. Planet\n", answer = "B" });
-        allThemePools[1].Add(new QuestionData { question = "Q1: What is 10+10?", answer = "20" });
-        allThemePools[1].Add(new QuestionData { question = "Q1: What is 10+10?", answer = "20" });
-        allThemePools[1].Add(new QuestionData { question = "Q1: What is 10+10?", answer = "20" });
-        allThemePools[1].Add(new QuestionData { question = "Q1: What is 10+10?", answer = "20" });
-        allThemePools[1].Add(new QuestionData { question = "Q1: What is 10+10?", answer = "20" });
-        allThemePools[1].Add(new QuestionData { question = "Q1: What is 10+10?", answer = "20" });
-        allThemePools[1].Add(new QuestionData { question = "Q1: What is 10+10?", answer = "20" });
-        allThemePools[1].Add(new QuestionData { question = "Q1: What is 10+10?", answer = "20" });
-        allThemePools[1].Add(new QuestionData { question = "Q1: What is 10+10?", answer = "20" });
-        allThemePools[1].Add(new QuestionData { question = "Q1: What is 10+10?", answer = "20" });
-        allThemePools[1].Add(new QuestionData { question = "Q1: What is 10+10?", answer = "20" });
-        allThemePools[1].Add(new QuestionData { question = "Q1: What is 10+10?", answer = "20" });
-        allThemePools[1].Add(new QuestionData { question = "Q1: What is 10+10?", answer = "20" });
-        allThemePools[1].Add(new QuestionData { question = "Q1: What is 10+10?", answer = "20" });
+        allThemePools[1].Add(new QuestionData { question = "Is Pluto considered a Planet?\n\nA. Yes\nB. No\n", answer = "B" });
+        allThemePools[1].Add(new QuestionData { question = "What year did World War III occur? \n\nA. 2067 \nB. 2107", answer = "A" });
+        allThemePools[1].Add(new QuestionData { question = "Who is the current president of Mars?\n\nA. Eli Moss \nB. Ronald Grump\nC. GrokX VII", answer = "A" });
+        allThemePools[1].Add(new QuestionData { question = "Complete the first line to our planetary anthem: Glory to ____\n\nA. Astortskan\nB. Our Orange Skies\nC. Ares", answer = "C" });
+        allThemePools[1].Add(new QuestionData { question = "What is the name of Mars' largest Moon?\n\nA.Phobos\nB.Deimos\nC.Thethes", answer = "A" });
+        allThemePools[1].Add(new QuestionData { question = "Did the current President vote in support of the immigration in Crisis act of 2070?\n\nA.Yes?\nB. No", answer = "A" });
+        
 
         // Theme 3 15 questions
-        allThemePools[2].Add(new QuestionData { question = "Q1: What color is the sky?", answer = "blue" });
-        allThemePools[2].Add(new QuestionData { question = "Q1: What color is the sky?", answer = "blue" });
-        allThemePools[2].Add(new QuestionData { question = "Q1: What color is the sky?", answer = "blue" });
-        allThemePools[2].Add(new QuestionData { question = "Q1: What color is the sky?", answer = "blue" });
-        allThemePools[2].Add(new QuestionData { question = "Q1: What color is the sky?", answer = "blue" });
-        allThemePools[2].Add(new QuestionData { question = "Q1: What color is the sky?", answer = "blue" });
-        allThemePools[2].Add(new QuestionData { question = "Q1: What color is the sky?", answer = "blue" });
-        allThemePools[2].Add(new QuestionData { question = "Q1: What color is the sky?", answer = "blue" });
-        allThemePools[2].Add(new QuestionData { question = "Q1: What color is the sky?", answer = "blue" });
-        allThemePools[2].Add(new QuestionData { question = "Q1: What color is the sky?", answer = "blue" });
-        allThemePools[2].Add(new QuestionData { question = "Q1: What color is the sky?", answer = "blue" });
-        allThemePools[2].Add(new QuestionData { question = "Q1: What color is the sky?", answer = "blue" });
-        allThemePools[2].Add(new QuestionData { question = "Q1: What color is the sky?", answer = "blue" });
-        allThemePools[2].Add(new QuestionData { question = "Q1: What color is the sky?", answer = "blue" });
-        allThemePools[2].Add(new QuestionData { question = "Q1: What color is the sky?", answer = "blue" });
+        allThemePools[2].Add(new QuestionData { question = "Do you consent to a background check, which is mandatory to recieve a visa for Mars?\n\nA.Yes\n B. No", answer = "A" });
+        allThemePools[2].Add(new QuestionData { question = "Do you consent to the monitoring of your digital footprint and habits?\n A.Yes \n B.No", answer = "A" });
+        allThemePools[2].Add(new QuestionData { question = "Do you consent to your answers and likeness being used to train artificial intelligences? \n A.Yes \n B.No", answer = "A" });
+        allThemePools[2].Add(new QuestionData { question = "Do you recognize that through recieving a visa, you forfeit your right to private medical records? \n A.Yes \n B.No", answer = "A" });
     }
 
     public void SwitchToTheme(int themeIndex)
@@ -154,7 +125,6 @@ public class QuizManager : MonoBehaviour
             {
                 finalScore = correctAnswersCount;
                 feedbackText.text = "You have completed all the questions.";
-                finalExitButton.gameObject.SetActive(true);
             }
         }
         else
