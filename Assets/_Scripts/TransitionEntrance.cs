b47f85795631e7fae37331ce42f29f55746c48ec
Eli Newsham <newshameli@gmail.com>
2025-10-12 08:34:05 -0400

    edited for build settings

--- Assets/_Scripts/TransitionEntrance.cs
+++ Assets/_Scripts/TransitionEntrance.cs
@@ -1,17 +1,17 @@
-using UnityEditor.SearchService;
+//using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
 
 public class TransitionEntrance : MonoBehaviour
 {
-    public string sceneLoad;
+  public string sceneLoad;
 
-    void OnTriggerEnter2D(Collider2D collision)
+  void OnTriggerEnter2D(Collider2D collision)
+  {
+    if (collision.gameObject.CompareTag("Player"))
     {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            SceneManager.LoadScene(sceneLoad);
-        }
+      SceneManager.LoadScene(sceneLoad);
     }
+  }
 }
--- Assets/_Scripts/playerMove.cs
+++ Assets/_Scripts/playerMove.cs
@@ -6,94 +6,94 @@ using UnityEngine.Rendering.Universal;
 
 public class playerMove : MonoBehaviour
 {
-    //sprite stuff with colors
-    public SpriteRenderer spriteRenderer;
-    public Sprite greenMode;
-    public Sprite yellowMode;
-    public Sprite redMode;
-    private LightBlink blinkScript;
-    public Light2D blinker;
+  //sprite stuff with colors
+  public SpriteRenderer spriteRenderer;
+  public Sprite greenMode;
+  public Sprite yellowMode;
+  public Sprite redMode;
+  private LightBlink blinkScript;
+  public Light2D blinker;
 
-    //rigidbody and movement
-    private Rigidbody2D rb;
-    public float speed = 20f;
-    float xInput;
-    float yInput;
+  //rigidbody and movement
+  private Rigidbody2D rb;
+  public float speed = 20f;
+  float xInput;
+  float yInput;
 
-    //variable for battery
-    public float batteryLevel;
+  //variable for battery
+  public float batteryLevel;
 
-    public static playerMove Instance { get; private set; }
+  public static playerMove Instance { get; private set; }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+  // Start is called once before the first execution of Update after the MonoBehaviour is created
+  void Start()
+  {
+    if (Instance == null)
     {
-        if (Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else if (Instance != this)
-        {
-            Destroy(gameObject);
-        }
-        rb = GetComponent<Rigidbody2D>();//set rigidbody to component
-        spriteRenderer = GetComponent<SpriteRenderer>(); //set sprite renderer to component
-        blinkScript = blinker.GetComponent<LightBlink>();//gets the script from blinking light
+      Instance = this;
+      DontDestroyOnLoad(gameObject);
     }
-
-    // Update is called once per frame
-    void FixedUpdate()
+    else if (Instance != this)
     {
-        ProcessBattery();
+      Destroy(gameObject);
     }
+    rb = GetComponent<Rigidbody2D>();//set rigidbody to component
+    spriteRenderer = GetComponent<SpriteRenderer>(); //set sprite renderer to component
+    blinkScript = blinker.GetComponent<LightBlink>();//gets the script from blinking light
+  }
 
-    void Update()
-    {
-        getInput();
-        MoveWithInput();
-    }
+  // Update is called once per frame
+  void FixedUpdate()
+  {
+    ProcessBattery();
+  }
 
-    void MoveWithInput()
-    {
-        if (Mathf.Abs(xInput) > 0)
-        {
-            rb.linearVelocity = new Vector2(xInput * speed, rb.linearVelocity.y);
-            float direction = Mathf.Sign(xInput);
-            transform.localScale = new Vector3(direction, 1, 1);
-        }
-        if (Mathf.Abs(yInput) > 0)
-        {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, yInput * speed);
-        }
-    }
+  void Update()
+  {
+    getInput();
+    MoveWithInput();
+  }
 
-    void getInput()
+  void MoveWithInput()
+  {
+    if (Mathf.Abs(xInput) > 0)
     {
-        xInput = Input.GetAxis("Horizontal");
-        yInput = Input.GetAxis("Vertical");
+      rb.linearVelocity = new Vector2(xInput * speed, rb.linearVelocity.y);
+      float direction = Mathf.Sign(xInput);
+      transform.localScale = new Vector3(direction, 1, 1);
     }
+    if (Mathf.Abs(yInput) > 0)
+    {
+      rb.linearVelocity = new Vector2(rb.linearVelocity.x, yInput * speed);
+    }
+  }
 
-    void ProcessBattery()//updates sprite depending on battery level
+  void getInput()
+  {
+    xInput = Input.GetAxis("Horizontal");
+    yInput = Input.GetAxis("Vertical");
+  }
+
+  void ProcessBattery()//updates sprite depending on battery level
+  {
+    //if battery is less than 700, turn yellow
+    if (batteryLevel < 700 && batteryLevel > 300)
     {
-        //if battery is less than 700, turn yellow
-        if (batteryLevel < 700 && batteryLevel > 300)
-        {
-            spriteRenderer.sprite = yellowMode;
-            blinker.color = Color.yellow;
-            blinkScript.setInterval(0.5f);
-        }
-        //if battery is less than 300, turn red
-        else if (batteryLevel <= 300)
-        {
-            spriteRenderer.sprite = redMode;
-            blinker.color = Color.red;
-            blinkScript.setInterval(0.25f);
-        }
-        //keep battery green
-        else
-        {
-            spriteRenderer.sprite = greenMode;
-        }
+      spriteRenderer.sprite = yellowMode;
+      blinker.color = Color.yellow;
+      blinkScript.setInterval(0.5f);
     }
+    //if battery is less than 300, turn red
+    else if (batteryLevel <= 300)
+    {
+      spriteRenderer.sprite = redMode;
+      blinker.color = Color.red;
+      blinkScript.setInterval(0.25f);
+    }
+    //keep battery green
+    else
+    {
+      spriteRenderer.sprite = greenMode;
+    }
+  }
 }
